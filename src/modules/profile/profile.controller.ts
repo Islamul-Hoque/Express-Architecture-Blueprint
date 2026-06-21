5d24ea0909152ebf588c7e3dc164397a2f21af7c
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-22 03:21:42 +0600

    feat(profile): implement createProfile controller to handle profile creation via service layer

--- src/modules/profile/profile.controller.ts
+++ src/modules/profile/profile.controller.ts
@@ -2,22 +2,22 @@ import type { Request, Response } from "express";
 import { profileService } from "./profile.service";
 
 const createProfile = async (req: Request, res: Response) => {
-    // try {
-    //     const result = await profileService.createProfileIntoDB(req.body);
+    try {
+        const result = await profileService.createProfileIntoDB(req.body);
 
-    //     // Response
-    //     res.status(201).json({
-    //         success: true,
-    //         message: "Profile created successfully!",
-    //         data: result.rows[0],
-    //     });
-    // } catch (error: any) {
-    //     res.status(500).json({
-    //         success: false,
-    //         message: error.message,
-    //         error: error,
-    //     });
-    // }
+        // Response
+        res.status(201).json({
+            success: true,
+            message: "Profile created successfully!",
+            data: result.rows[0],
+        });
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            error: error,
+        });
+    }
 };
 
 export const profileController = {
