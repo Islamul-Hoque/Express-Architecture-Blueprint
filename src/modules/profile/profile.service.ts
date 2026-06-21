ccacaccb7315cdf008b7c78a6443631e4958b584
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-22 03:22:47 +0600

    feat(profile): implement createProfileIntoDB service with user existence check and profile insertion

--- src/modules/profile/profile.service.ts
+++ src/modules/profile/profile.service.ts
@@ -1,28 +1,28 @@
 import { pool } from "../../db";
 
 const createProfileIntoDB = async (payload: any) => {
-//     const { user_id, bio, address, phone, gender } = payload;
+    const { user_id, bio, address, phone, gender } = payload;
 
-//     // First check if the user is exists
-//     const user = await pool.query(`
-//         SELECT * FROM users WHERE id=$1
-//     `, [user_id],
-//     );
+    // First check if the user is exists
+    const user = await pool.query(`
+        SELECT * FROM users WHERE id=$1
+    `, [user_id],
+    );
 
-//     // User not found
-//     if (user.rows.length === 0) {
-//         throw new Error("User not exists!");
-//     }
+    // User not found
+    if (user.rows.length === 0) {
+        throw new Error("User not exists!");
+    }
 
-//     const result = await pool.query( `
-//         INSERT INTO 
-//             profiles(user_id, bio, address, phone, gender) 
-//             VALUES($1,$2,$3,$4,$5) 
-//         RETURNING *
-//     `,
-//         [user_id, bio, address, phone, gender],
-//     );
-//     return result;
+    const result = await pool.query( `
+        INSERT INTO 
+            profiles(user_id, bio, address, phone, gender) 
+            VALUES($1,$2,$3,$4,$5) 
+        RETURNING *
+    `,
+        [user_id, bio, address, phone, gender],
+    );
+    return result;
 };
 
 export const profileService = {
