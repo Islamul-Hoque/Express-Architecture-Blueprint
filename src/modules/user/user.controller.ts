f78df6e1445591bcfc3f9ea445d94733c0be4433
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-22 02:09:16 +0600

    feat(user.interface.ts): add IUser interface to define user data structure with type safety

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,17 +1,13 @@
 import type { Request, Response } from "express";
 import { pool } from "../../db";
+import { userService } from "./user.service";
 
 // New user Create ==> "POST" 
 const createUser = async (req: Request, res: Response) => {
     const { name, email, password, age } = req.body
 
     try {
-        const result = await pool.query(`
-            INSERT INTO users(name,email,password,age) VALUES($1,$2,$3,$4) 
-            RETURNING *
-            `,
-            [name, email, password, age],
-        );
+        const result = await userService.createUserIntoDB(req.body);
 
         res.status(201).json({
             success: true,
@@ -30,9 +26,7 @@ const createUser = async (req: Request, res: Response) => {
 // "GET" All User
 const getAllUsers = async (req: Request, res: Response) => {
     try {
-        const result = await pool.query(`
-            SELECT * FROM users  
-        `);
+        const result = await userService.getAllUsersFromDB()
         res.status(200).json({
             success: true,
             message: "Users retrieved successfully!",
@@ -52,10 +46,7 @@ const getSingleUser = async (req: Request, res: Response) => {
     const { id } = req.params;
 
     try {
-        const result = await pool.query(`
-            SELECT * FROM users WHERE id=$1  
-        `, [id],
-        );
+        const result = await userService.getSingleUserFromDB(id as string);
 
         if (result.rows.length === 0) {
             res.status(404).json({
@@ -85,13 +76,7 @@ const updateUser = async (req: Request, res: Response) => {
     const { name, password, age, is_active } = req.body;
 
     try {
-        const result = await pool.query(`
-            UPDATE users 
-            SET name=COALESCE($1,name), password=COALESCE($2,password), age=COALESCE($3,age), is_active=COALESCE($4,is_active) 
-            WHERE id=$5 RETURNING *
-            `,
-            [name, password, age, is_active, id],
-        );
+        const result = await userService.updateUserFromDB(req.body, id as string);
 
         // if id not found
         if (result.rows.length === 0) {
@@ -120,11 +105,7 @@ const deleteUser = async (req: Request, res: Response) => {
     const { id } = req.params;
 
     try {
-        const result = await pool.query( `
-            DELETE FROM users WHERE id=$1 
-        `,
-        [id],
-        );
+        const result = await userService.deleteUserFromDB(id as string);
 
         // if id not found
         if (result.rowCount === 0) {
