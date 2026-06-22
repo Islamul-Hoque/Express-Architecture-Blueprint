6120a6c9bf1a648811cb4590899ca15bc08153aa
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-23 00:52:31 +0600

    feat(auth/auth.controller.ts): implement loginUser controller to handle user login via service layer

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,22 +2,22 @@ import type { Request, Response } from "express";
 import { authService } from "./auth.service";
 
 const loginUser = async (req: Request, res: Response) => {
-    // try {
-    //     const result = await authService.loginUserIntoDB(req.body);
+    try {
+        const result = await authService.loginUserIntoDB(req.body);
 
-    //     // Response
-    //     res.status(200).json({
-    //         success: true,
-    //         message: "User retrieved successfully!",
-    //         data: result,
-    //     });
-    // } catch (error: any) {
-    //     res.status(500).json({
-    //         success: false,
-    //         message: error.message,
-    //         error: error,
-    //     });
-    // }
+        // Response
+        res.status(200).json({
+            success: true,
+            message: "User retrieved successfully!",
+            data: result,
+        });
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            error: error,
+        });
+    }
 };
 
 export const authController = {
