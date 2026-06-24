fa8fb11a1df6307106ee93a2ed05110480a94dd8
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-24 13:48:12 +0600

    feat(types): define USER_ROLE constant and ROLES type for role-based authorization

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,7 @@
-// export const USER_ROLE = {
-//     admin: "admin",
-//     agent: "agent",
-//     user: "user",
-// } as const;
+export const USER_ROLE = {
+    admin: "admin",
+    agent: "agent",
+    user: "user",
+} as const;
 
-// export type ROLES = "admin" | "agent" | "user";
+export type ROLES = "admin" | "agent" | "user";
